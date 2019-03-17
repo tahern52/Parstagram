4e19be61fa3703a7e3cf2f7a823b99a69988b275
Taher Naeem <tahern52@cs.utexas.edu>
2019-03-17 15:49:12 -0500

    Added capture mode

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Taher on 3/17/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var commentField: UITextField!
+    let sourceDialog = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        sourceDialog.addAction(UIAlertAction(title: "Launch camera", style: .default, handler: { (UIAlertAction) in
+            self.sourceDialog.dismiss(animated: true, completion: {
+                let picker = UIImagePickerController()
+                picker.delegate = self
+                picker.allowsEditing = true
+                picker.sourceType = .camera
+                
+                self.present(picker, animated: true, completion: nil)
+            })
+        }))
+        sourceDialog.addAction(UIAlertAction(title: "Choose from Photo Library", style: .default, handler: { (UIAlertAction) in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = .photoLibrary
+            
+            self.present(picker, animated: true, completion: nil)
+        }))
+    }
+    
+
+    @IBAction func onCapture(_ sender: Any) {
+        present(sourceDialog, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,65 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Taher on 3/17/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                print("An error occurred: \(error?.localizedDescription ?? "No error data available")")
+            }
+        }
+    }
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        // Alert dialog for invalid sign in attempt
+        let invalidLoginAlert = UIAlertController(title: "Error", message: "Invalid username or password", preferredStyle: .alert)
+        invalidLoginAlert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        
+        // Attempt log in
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                self.present(invalidLoginAlert, animated: true)
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
